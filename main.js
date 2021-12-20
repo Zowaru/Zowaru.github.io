90888a19954380fc487c86b275d987db3c5071e1
Zanzigu <74876844+Zanzigu@users.noreply.github.com>
2021-12-20 15:34:36 +0100

    app offline

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 // questo file verrà eseguito su TUTTE le pagine
 
+// avvia service-worker
+if ('serviceWorker' in navigator) 
+{
+  window.addEventListener('load', async () => {
+    try 
+    {
+      const reg = await navigator.serviceWorker.register('/service-worker.js');
+      console.log('Service worker avviato. ', reg);
+    } 
+    catch (err) 
+    {
+      console.log('registrazione service worker fallita - E: ', err);
+    }
+
+  });
+}
+
+
+// posizionamento iniziale componenti
 document.getElementById('contenitoreBottoni').style.bottom = '0';
 document.getElementById('linkBottoni').style.bottom = '3.1em';
 document.getElementById('container').style.bottom = 'calc(-50% + 1rem)';
 document.getElementById('container').style.transform = 'translateY(-50%)';
+
+
+// mostra in base al giorno
 
 
 function animazioneContainer()
@@ -13,3 +35,4 @@ function animazioneContainer()
         document.getElementById('container').style.bottom = 'calc(-50% + 1rem)';
     }, 500);
 }
+
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+// questo file viene avviato nel caso in cui l' "app" venga aperta in modalità offline
+
+// nome per istanza di cache
+const cacheName = 'cache-v1';
+
+// lista dei file in pre-cache
+const precacheResources = ['/', '/index.html', '/stile/style.css', '/main.js', '/img/clock.png'];
+
+// mentre "installa" il sito, scarica la lista dei file nella pre-cache
+self.addEventListener('install', (event) => {
+  console.log('Installazione sito...');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// per qualsiasi richiesta di fetch, prova a rispondere con le risorse in cache
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      
+      return fetch(event.request);
+    }),
+  );
+});
